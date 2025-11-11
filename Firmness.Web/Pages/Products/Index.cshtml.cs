d9e7344e296ea58ca8cf345b02435cbfb8546b56
armando maikol duran salas <maikold774@gmail.com>
2025-11-10 19:47:34 -0500

    feat: the crud for project was created

---
+++ Firmness.Web/Pages/Products/Index.cshtml.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Firmness.Domain.Entities;
+using Firmness.Infraestructure.Data;
+
+namespace Firmness.Web.Pages.Products
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Firmness.Infraestructure.Data.ApplicationDbContext _context;
+
+        public IndexModel(Firmness.Infraestructure.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Product> Product { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            Product = await _context.Products.ToListAsync();
+        }
+    }
+}
